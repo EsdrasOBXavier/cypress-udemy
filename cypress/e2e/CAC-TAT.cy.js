310042de8d202eb0e18ba0013846c4c915379696
Esdras Oliveira <esdras_obrainer@hotmail.com>
2026-03-03 10:07:33 -0300

    Lesson 02 - Get, Type and Click

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,19 @@
 describe('Central de Atendimento', () => {
-  it('verifica título', () => {
+   beforeEach(() => {
     cy.visit('./src/index.html')
-
+  })
+  it('verifica título', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
+
+  it('preencher campos obrigatórios', () => {
+     cy.get('#firstName').type('Lua')
+     cy.get('#lastName').type('Nova')
+     cy.get('#email').type('lua.nova@gmail.com')
+     cy.get('#phone').type('(81)99999-9999')
+     cy.get('#open-text-area').type('Isto é apenas um teste')
+     cy.get('.button[type="submit"]').click()
+     cy.get('.success').should('be.visible')
+  })
 })
+
